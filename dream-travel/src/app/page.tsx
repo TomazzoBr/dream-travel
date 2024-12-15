1434597ca6f823b57c3463d67ab58f5a618c8ccb
Tomazzo Antonioli <tomazzo10@hotmail.com>
2024-12-15 13:52:18 +0100

    update page.tsx component

---
+++ dream-travel/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Landing from "./components/landing/landing";
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] w-full items-center justify-items-center min-h-screen pb-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col w-full gap-8 row-start-2 items-center sm:items-start">
+        <Landing />
+      </main>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
+    </div>
+  );
+}
