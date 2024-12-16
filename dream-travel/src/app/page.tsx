2d54cc0d7007f95cd3f2e6025fcd316ba3c8299a
Tomazzo Antonioli <tomazzo10@hotmail.com>
2024-12-17 00:24:26 +0100

    fix rows config

--- dream-travel/src/app/page.tsx
+++ dream-travel/src/app/page.tsx
@@ -2,7 +2,7 @@ import Landing from "./components/landing/landing";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] w-full items-center justify-items-center min-h-screen pb-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[1fr] w-full items-center justify-items-center min-h-screen pb-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col w-full gap-8 row-start-2 items-center sm:items-start">
         <Landing />
       </main>
